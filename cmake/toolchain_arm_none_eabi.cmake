cb6738d17b3fc11d8df975ccab4cd5b8118d0e04
MartinK7 <martinelekt@seznam.cz>
2022-01-06 23:38:59 +0100

    Update: Project is now converted to Makefile project

---
+++ cmake/toolchain_arm_none_eabi.cmake
@@ -0,0 +1,35 @@
+################################################################################
+##                              TOOLCHAIN                                     ##
+################################################################################
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_C_COMPILER "arm-none-eabi-gcc")
+set(CMAKE_CXX_COMPILER "arm-none-eabi-g++")
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+set_property(SOURCE ${SOURCES} PROPERTY LANGUAGE C)
+
+################################################################################
+##                              COMPILER                                      ##
+################################################################################
+
+add_executable(${TARGET} ${SOURCES})
+target_compile_definitions(${TARGET} PUBLIC ${DEFINES})
+target_include_directories(${TARGET} PUBLIC ${INCLUDES})
+target_compile_options(${TARGET} PUBLIC ${CFLAGS})
+target_link_options(${TARGET} PUBLIC ${LFLAGS})
+target_link_libraries(${TARGET} ${LIBRARIES})
+
+set_target_properties(${TARGET} PROPERTIES SUFFIX ".elf")
+
+add_custom_command(TARGET ${TARGET} POST_BUILD
+        COMMAND arm-none-eabi-size --format=berkeley --totals "$<TARGET_FILE:${TARGET}>"
+        COMMAND arm-none-eabi-objcopy -O binary "$<TARGET_FILE:${TARGET}>" "$<TARGET_FILE:${TARGET}>.bin"
+        COMMAND xxd "$<TARGET_FILE:${TARGET}>.bin" > "$<TARGET_FILE:${TARGET}>.bin.list"
+        COMMAND arm-none-eabi-objdump -d "$<TARGET_FILE:${TARGET}>" > "$<TARGET_FILE:${TARGET}>.list"
+)
+
